c49b7fc18875c8735390361568ef1095fb8a7fa7
Rafa <rafaelacristina2003@gmail.com>
2023-10-12 20:39:00 -0300

    meu primeiro commit

--- lib/telas/home/recompensas/recompensas_screen.dart
+++ lib/telas/home/recompensas/recompensas_screen.dart
@@ -9,8 +9,57 @@ class RecompensasScreen extends StatefulWidget {
 }
 
 class _RecompensasScreeenState extends State<RecompensasScreen> {
+
+    Widget _body(){
+      return ListView(
+        children:[
+          container(
+            height:100,
+            width: MediaQuery.of(context).size.width,
+            radius: BorderRadius.circular(10),
+            colors: Colors.gray.shade300,
+            child: Column(
+              children:[
+                text("Cashbacks via PIX podem demorar até 3 dias para serem depositados na conta do usuário, representando apenas 40% do valor dos pontos acumulados.")
+                Row(
+                  children:[
+                    text("R\$00,00"),
+                    elevatedButtonText("Solicitar valor",onPressed: ()=>{})
+                  ]
+                )
+              ]
+            )
+          ),
+          SizedBox(
+            height: 180,
+            child: ListView(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              children: cardsMaisTrocados,
+            ),
+          ),
+          SizedBox(
+            height: 180,
+            child: ListView(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              children: cardsMaisTrocados,
+            ),
+          ),
+          SizedBox(
+            height: 180,
+            child: ListView(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              children: cardsMaisTrocados,
+            ),
+          ),
+        ]
+      );
+    }
+
   @override
   Widget build(BuildContext context) {
-    return scaffold(body: Container(), title: "Recompensas");
+    return scaffold(body:_body(), title: "Recompensas");
   }
 }
